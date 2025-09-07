8faa3a82d5056609f5dbe320d7ff332c74fb2723
Marco Guzman <guzmanescobar3@gmail.com>
2025-09-07 11:19:08 +0200

    Introduce ShowFactory and adjust show creation

--- src/main/java/com/jad/show/ConcertShow.java
+++ src/main/java/com/jad/show/ConcertShow.java
@@ -3,7 +3,7 @@ package com.jad.show;
 public class ConcertShow extends Show {
     private final String artist;
 
-    public ConcertShow(final String name, final String description, final String artist) {
+    ConcertShow(final String name, final String description, final String artist) {
         super(name, description, ShowType.CONCERT);
         this.artist = artist;
     }
